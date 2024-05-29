09d94947ece35ec2e1a1d3a5ad7c19274cce2728
SynapticWhisper <vochernobrivets@gmail.com>
2024-05-29 13:12:01 +0300

    fix

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi.responses import RedirectResponse
 from starlette.middleware.base import BaseHTTPMiddleware
 from src.auth.router import router as AuthRouter
 from src.users.router import router as UserRouter
+
 app = FastAPI(
     title="ToDo-API",
     version="0.2.1"
@@ -17,6 +18,7 @@ class RedirectMiddleware(BaseHTTPMiddleware):
         if response.status_code == 401:
             return RedirectResponse(url="/auth/v1/login")
         return response
+
 
 app.include_router(AuthRouter)
 app.include_router(UserRouter)
